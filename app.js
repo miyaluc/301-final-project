74c025e46c4b963c12354c456ec7164099fde83d
miyaluc <srlucas3@gmail.com>
2017-09-05 19:48:15 -0700

    having trouble accessing dark sky API

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// make click function and put API request inside of it - function that runs when hitting the button that makes request for me
+
+$('#click-me').on('click', function(e) {
+    e.preventDefault();
+    $.getJSON('https://api.darksky.net/forecast/e8130e2e58e8b8f752de9220cb7c2e8e/37.8267,-122.4233&callback=?', function(forecast) {
+      console.log(forecast);
+      // .then(function) {
+        //do something, maybe append to page
+      // };
+    });
+  })
+// }
+
+// letsPlay();
