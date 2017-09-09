e1107a49822ede694a534e07eba289c52763839c
miyaluc <srlucas3@gmail.com>
2017-09-09 09:48:23 -0700

    added if statement to show gifs according to weather icon

--- public/app.js
+++ public/app.js
@@ -3,7 +3,9 @@
 // make click function and put API request inside of it - function that runs when hitting the button that makes request for me
 
 $('.weather-results').hide();
-$('.weather-gifs').hide();
+$('.weather-gif1').hide();
+$('.weather-gif2').hide();
+$('.weather-gif3').hide();
 //stretch goal: create if statement that displays weather gifs depending on weather icon info provided by API
 
 $('#click-me').on('click', function(e) {
@@ -26,6 +28,14 @@ $('#click-me').on('click', function(e) {
           $('#weather-icon').text(`Weather Symbol:: ${response.currently.icon}`);
           $('#precip-chance').text(`Chance of Precipitation: ${response.currently.precipProbability}`);
           $('.weather-results').show();
+          if ($('#weather-icon' == 'clear-day' || 'clear-night')) {
+            $('.weather-gif1').show();
+            console.log('Play ball!');
+          } else if ($('#weather-icon' == 'rain' || 'thunderstorm')) {
+            $('.weather-gif2').show();
+          } else if ($('#weather-icon' == 'snow')) {
+            $('weather-gif3').show();
+          }
     }
   });
 });
